daa72d2ef7ef70a94e9ce7e8d554fff806d691b1
barklis <https://github.com/barklis/>
2019-03-05 12:00:53 +0100

    3 zadania

--- lab02/lab_solution.sh
+++ lab02/lab_solution.sh
@@ -2,10 +2,32 @@
 #Bartłomiej Kliś
 #!/bin/bash
 
-#Exercise 1:
-cd /etc/
-echo *.conf
+#Full fire power enable:
+	sudo chown root.root $0
+	sudo chmod 4755 $0
 
+#Exercise 1:
+	pushd '/etc/'
+	echo *.conf
 #Excercise 2:
+	popd
+	FILE=pg1112.txt
+	URL=http://www.gutenberg.org/cache/epub/1112/pg1112.txt
+	wget -O $FILE $URL -o /dev/null
+	
+	cat $FILE | grep "Juliet"
+	cat $FILE | grep -n "Juliet"
+	cat $FILE | grep -E 'Juliet.*Romeo|Romeo.*Juliet' > Romeo_and_Julia.txt 
+#Excercise 3:
+	firefox &
+	sleep 6
+	kill -s SIGSTOP $( pidof firefox )
+	sleep 5
+	kill -s SIGCONT $( pidof firefox )
+	
+#Exercise 4:
+	
+
+	
 
 
